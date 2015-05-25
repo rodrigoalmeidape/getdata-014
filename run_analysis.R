39b91177c2de052fcaf3dd60c6d14d92172f4470
Rodrigo Almeida <rodrigo.pe@gmail.com>
2015-05-25 00:05:02 -0300

    Add a more efficient way to read files.
    read.table instead of read.csv.

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,32 @@
-#############################################
-# Load datasets
-#############################################
+# Load "mean" and "standard deviation" features
+features <- read.table("features.txt", col.names=c("FeatureID", "FeatureName"), header=F, as.is=T)
+features_ms <- features[grep('mean|std',features$FeatureName),]
 
-# Subjects Values (subject_id)
-subject_test <- read.csv(file = "test/subject_test.txt", header = FALSE, sep=' ')
-subject_train <- read.csv(file = "train/subject_train.txt", header = FALSE, sep=' ')
-# Activities Values (activity_id)
-y_train <- read.csv(file = "train/y_train.txt", header = FALSE, sep=' ')
-y_test <- read.csv(file = "test/y_test.txt", header = FALSE, sep=' ')
-# Activity Labels (eg.: 1 WALKING, 2 WALKING_UPSTAIRS, 3 WALKING_DOWNSTAIRS, 4 SITTING, 5 STANDING, 6 LAYING)
-activity_labels <- read.csv(file = "activity_labels.txt", header = FALSE, sep=' ')
-names(activity_labels) <- c("activity_id", "activity_desc")
-# Features (eg.: 1 tBodyAcc-mean()-X, 2 tBodyAcc-mean()-Y, 3 tBodyAcc-mean()-Z), etc)
-features <- read.csv(file = "activity_labels.txt", header = FALSE, sep=' ')
-names(features) <- c("feature_id", "feature_desc")
+# Load the activity labels
+activity_labels <- read.table("activity_labels.txt", header=F, as.is=T)
+colnames(activity_labels) <- c("ActivityID", "ActivityName")
 
-#############################################
-# Merge Datasets 
-#############################################
+# Load the subjects (An ID which represents a volunteer)
+data_subject_test <- read.table("test/subject_test.txt", header=F, as.is=T)
+data_subject_train <- read.table("train/subject_train.txt", header=F, as.is=T)
+data_subject <- rbind(data_subject_test, data_subject_train)
+colnames(data_subject) <- c("Subject")
 
-# Subjects (test + train)
-subjects <- rbind(subject_test, subject_train)
-names(subjects) = c("subject_id")
-# Activities (test + train)
-activities <- rbind(y_test, y_train)
-names(activities) = c("activity_id")
-# Subjects + Activities
-res <- cbind(subjects, activities)
-temp_tidy <- sqldf('select r.subject_id subject, al.activity_desc activity from res r, activity_labels al where r.activity_id = al.activity_id')
+# Load measurements (just "mean" and "standard deviation")
+data_x_test <- read.table(file = "test/X_test.txt")
+data_x_train <- read.table(file = "train/X_train.txt")
+data_x <- rbind(data_x_test, data_x_train)
+colnames(data_x) <- features$FeatureName
+data_x_subset <- data_x[,features_ms$FeatureName]
 
+# Load the activities related to each measurement
+data_y_test <- read.table(file = "test/Y_test.txt")
+data_y_train <- read.table(file = "train/Y_train.txt")
+data_y <- rbind(data_y_test, data_y_train)
+colnames(data_y) <- c("ActivityID")
+data_y_named <- merge(data_y, activity_labels)
+data_y_named$ActivityID = NULL
+
+# Merge all datasets
+tidy_data <- cbind(data_subject, data_y_named, data_x_subset)
 
